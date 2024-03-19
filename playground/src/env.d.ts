cd2d3b9481e1e1b542719870db89988b58baa740
Adam Matthiesen <amatthiesen@outlook.com>
2024-03-19 03:59:27 -0700

    Update tsconfig.json files

--- playground/src/env.d.ts
+++ playground/src/env.d.ts
@@ -1,5 +1,4 @@
-/// <reference path="../.astro/db-types.d.ts" />
 /// <reference path="../.astro/types.d.ts" />
+/// <reference path="../.astro/db-types.d.ts" />
 /// <reference types="astro/client" />
 /// <reference types="../.astro/virtual:astro-studio-cms.d.ts" />
-/// <reference types="../.astro/astro-studio-cms.d.ts" />
