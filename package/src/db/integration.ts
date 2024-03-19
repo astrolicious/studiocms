56fc707c1c8e01ccb14bd11abb391bce7783d7a3
Adam Matthiesen <amatthiesen@outlook.com>
2024-03-18 19:40:58 -0700

    adds resolver to db integration, and removes exports from package.json

--- package/src/db/integration.ts
+++ package/src/db/integration.ts
@@ -1,23 +1,25 @@
 import { defineDbIntegration } from '@astrojs/db/utils';
 import { integrationLogger } from '../utils';
+import { createResolver } from "astro-integration-kit";
 
 // This is the integration that will be used to link the database to the Astro Studio CMS
 /**
  * AstroStudioCMS Database config & seed Integration
  */
 export default function StudioCMS() {
+    const { resolve } = createResolver(import.meta.url);
+
     return defineDbIntegration({
         name: 'astro-studio-cms-db',
         hooks: {
             'astro:db:setup': ({ extendDb }) => {
                 extendDb({
-                    configEntrypoint: '@nametbd/astro-studio-cms/config.ts',
-                    seedEntrypoint: '@nametbd/astro-studio-cms/seed.ts',
+                    configEntrypoint: resolve('./config.ts'),
+                    seedEntrypoint: resolve('./seed.ts'),
                 });
             },
             'astro:config:setup': ({ logger }) => {
                 integrationLogger(logger, true, 'info', 'Custom AstroDB configuration loaded!');
-
             }
         },
     });
