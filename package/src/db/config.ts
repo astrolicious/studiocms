a1b9aedd8f5f44c198f90dc073420d7ccda10ad1
Adam Matthiesen <amatthiesen@outlook.com>
2024-03-26 23:47:51 -0700

    Remove Admins table from db config

--- package/src/db/config.ts
+++ package/src/db/config.ts
@@ -1,6 +1,6 @@
 // @ts-expect-error - This is a missing type definition for the `astro:db` import since its a virtual module during Astro Runtime
 import { defineDb } from "astro:db";
-import { Blog, Pages, Session, User, SiteConfig, Admins, Permissions } from './tables';
+import { Blog, Pages, Session, User, SiteConfig, Permissions } from './tables';
 
 // https://astro.build/db/config
 export default defineDb({
@@ -8,7 +8,6 @@ export default defineDb({
 		SiteConfig,
 		Blog, 
 		Pages, 
-		Admins,
 		Session, 
 		User, 
 		Permissions,
