47ebff53e6b607125654a44aa626f821eb942cf5
Adam Matthiesen <amatthiesen@outlook.com>
2024-03-27 20:23:56 -0700

    Fix environment variable fallback in CImage and auth.ts

--- package/src/lib/auth.ts
+++ package/src/lib/auth.ts
@@ -7,8 +7,8 @@ import { GitHub } from "arctic";
 import { Session, User } from "../db/tables";
 
 export const github = new GitHub(
-	import.meta.env.CMS_GITHUB_CLIENT_ID,
-	import.meta.env.CMS_GITHUB_CLIENT_SECRET,
+	import.meta.env.CMS_GITHUB_CLIENT_ID || process.env.CMS_GITHUB_CLIENT_ID,
+	import.meta.env.CMS_GITHUB_CLIENT_SECRET || process.env.CMS_GITHUB_CLIENT_SECRET,
 );
 
 const typeSafeSession = asDrizzleTable("Session", Session);
