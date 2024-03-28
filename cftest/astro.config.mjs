865744ff4cb88e801722157023bb68934e91402a
Adam Matthiesen <amatthiesen@outlook.com>
2024-03-28 15:03:57 -0700

    add testing CF project

---
+++ cftest/astro.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from "astro/config";
+import astroStudioCMS from "@astrolicious/studiocms";
+import db from '@astrojs/db';
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'http://localhost:4321',
+	output: "server",
+	adapter: cloudflare(),
+	integrations: [
+		db(),
+		astroStudioCMS({
+			dbStartPage: false,
+			verbose: true,
+		}),
+	],
+});
---
+++ cftest/src/env.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="../.astro/types.d.ts" />
+/// <reference path="../.astro/db-types.d.ts" />
+/// <reference types="astro/client" />
+/// <reference types="../.astro/virtual:astro-studio-cms.d.ts" />
