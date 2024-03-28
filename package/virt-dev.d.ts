0af6ea4c2b3e9d25404f06bc10f735a0a6399785
Adam Matthiesen <amatthiesen@outlook.com>
2024-03-28 00:12:07 -0700

    Update GitHub authentication in auth.ts and add ImportMetaEnv interface in virt-dev.d.ts

--- package/virt-dev.d.ts
+++ package/virt-dev.d.ts
@@ -2,3 +2,14 @@ declare module 'virtual:astro-studio-cms:config' {
     const Config: import("./src/schemas").AstroStudioCMSOptions;
      export default Config;
 }
+
+interface ImportMetaEnv {
+    readonly CMS_GITHUB_CLIENT_ID: string;
+    readonly CMS_GITHUB_CLIENT_SECRET: string;
+    readonly CMS_CLOUDINARY_CLOUDNAME: string;
+    readonly PROD: boolean;
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
