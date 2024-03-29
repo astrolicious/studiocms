8edc97a27f086b251054dab09e6514f82fba2bbf
Adam Matthiesen <amatthiesen@outlook.com>
2024-03-28 22:49:57 -0700

    test

--- cftest/astro.config.mjs
+++ cftest/astro.config.mjs
@@ -11,7 +11,7 @@ export default defineConfig({
 		imageService: "passthrough",
 		routes: {
 			extend: {
-				include: [{ pattern: "/*" }],
+				include: [{ pattern: "/start" }],
 			},
 		},
 		platformProxy: {
