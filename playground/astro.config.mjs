cd4851ba42d53d752e8c1d93bf72cac6a069fda5
Adam Matthiesen <amatthiesen@outlook.com>
2024-04-01 12:43:11 -0700

    Update astro.config.mjs to use vercel adapter

--- playground/astro.config.mjs
+++ playground/astro.config.mjs
@@ -2,13 +2,13 @@ import db from '@astrojs/db';
 // import node from '@astrojs/node';
 import astroStudioCMS from '@astrolicious/studiocms';
 import { defineConfig } from 'astro/config';
-import node from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://ikc4k48.astro-studiocms.xyz',
 	output: 'server',
-	adapter: node(),
+	adapter: vercel(),
 	integrations: [
 		db(),
 		astroStudioCMS({
