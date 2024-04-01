a344c896ad9032f4e534b76e246f8824d3729016
Adam Matthiesen <amatthiesen@outlook.com>
2024-04-01 09:01:21 -0700

    update

--- packages/studioCMS/src/utils/rankcheck.ts
+++ packages/studioCMS/src/utils/rankcheck.ts
@@ -3,21 +3,22 @@ import { Permissions, db, eq } from 'astro:db';
 import type { Locals } from '../pages/dashboard/locals';
 
 export const rankCheck = async (locals: Locals) => {
-	const permissions = await db
-		.select()
-		.from(Permissions)
-		.where(eq(Permissions.username, locals.user.username))
-		.catch(() => {
-			return false;
-		});
-	if (permissions[0] && permissions[0].username === locals.user.username) {
-		if (permissions[0].rank === 'admin') {
-			return 'admin';
-		}
-		if (permissions[0].rank === 'editor') {
-			return 'editor';
+	if (locals.isLoggedIn){
+		const permissions = await db
+			.select()
+			.from(Permissions)
+			.where(eq(Permissions.username, locals.user.username))
+			.catch(() => {
+				return false;
+			});
+		if (permissions[0] && permissions[0].username === locals.user.username) {
+			if (permissions[0].rank === 'admin') {
+				return 'admin';
+			}
+			if (permissions[0].rank === 'editor') {
+				return 'editor';
+			}
 		}
 	}
-
 	return 'visitor';
 };
