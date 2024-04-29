64155016f3ade002db9e8287dafa3f1f38b8d535
Adam Matthiesen <amatthiesen@outlook.com>
2024-04-29 07:25:51 -0700

    update link in jsdoc

--- packages/studioCMS/src/index.d.ts
+++ packages/studioCMS/src/index.d.ts
@@ -5,7 +5,7 @@ import type { StudioCMSOptions } from './schemas';
  * # Astro Studio CMS Integration
  * A CMS built for Astro by the Astro Community for the Astro Community.
  *
- * Checkout our [GitHub Repo `@astrolicious/studiocms`](https://github.com/astrolicious/)
+ * Checkout our [GitHub Repo `@astrolicious/studiocms`](https://github.com/astrolicious/studiocms)
  *
  * Check out [Astro-StudioCMS.xyz](https://astro-studiocms.xyz) or the Built-in JSDocs *(Hover Docs like this)* for more information.
  *
