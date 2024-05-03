f8a498f76ce4505416e4adf0cbe0a67ba1631f71
Adam Matthiesen <amatthiesen@outlook.com>
2024-05-03 05:54:01 -0700

    Overhaul to the auth helperscript to return an object containing user data used on multiple pages instead of just a string

---
+++ packages/studioCMS/src/utils/authhelper.ts
@@ -0,0 +1,65 @@
+// @ts-expect-error - Types are only available during runtime
+import { Permissions, db, eq } from 'astro:db';
+import type { Locals } from '../schemas/locals';
+import { lucia } from '../lib/auth';
+import type { Session } from 'lucia';
+
+type authHelperResponse = {
+    username: string|null;
+    name: string|null;
+    email: string|null;
+    avatar: string|null;
+    githubURL: string|null;
+    permissionLevel: 'admin'|'editor'|'visitor'|'unknown';
+    currentUserSession: Session|undefined;
+}
+
+export default async function authHelper(locals: Locals): Promise<authHelperResponse> {
+    const isLoggedIn = locals.isLoggedIn;
+    
+    if (isLoggedIn) {
+        const sessionArray = await lucia.getUserSessions(locals.dbUser.id);
+        const username = locals.dbUser.username;
+        const name = locals.dbUser.name;
+        const email = locals.dbUser.email;
+        const avatar = locals.dbUser.avatar;
+        const githubURL = locals.dbUser.githubURL;
+        const currentUserSession = sessionArray[0];
+        let permissionLevel: 'admin'|'editor'|'visitor'|'unknown' = 'unknown';
+        const permissions = await db
+            .select()
+            .from(Permissions)
+            .where(eq(Permissions.username, username))
+            .catch(() => { return false; });
+
+        if (permissions[0] && permissions[0].username === username) {
+            if (permissions[0].rank === 'admin') {
+                permissionLevel = 'admin';
+            }
+            if (permissions[0].rank === 'editor') {
+                permissionLevel = 'editor';
+            }
+            if (permissions[0].rank === 'visitor') {
+                permissionLevel = 'visitor';
+            }
+        }
+        return {
+            username,
+            name,
+            email,
+            avatar,
+            githubURL,
+            permissionLevel,
+            currentUserSession
+        }
+    }
+    return {
+        username: null,
+        name: null,
+        email: null,
+        avatar: null,
+        githubURL: null,
+        permissionLevel: 'unknown',
+        currentUserSession: undefined
+    }
+}
