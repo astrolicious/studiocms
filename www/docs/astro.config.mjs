27c62b037b755bff220ad770e2e29ace4656fcc9
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2024-05-07 20:53:56 -0600

    init docs

--- www/docs/astro.config.mjs
+++ www/docs/astro.config.mjs
@@ -1,37 +1,48 @@
-import { defineConfig } from 'astro/config'
-import starlight from '@astrojs/starlight'
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     starlight({
-      title: 'My Docs',
-      social: {
-        github: 'https://github.com/withastro/starlight',
+      title: "Astro StudioCMS",
+      favicon: "/logo-dark.svg",
+      logo: {
+        dark: "../assets/logo-light.svg",
+        light: "../assets/logo-dark.svg",
       },
+      social: {
+        github: "https://github.com/astrolicious/studiocms",
+      },
+      customCss: [
+        // Relative path to your custom CSS file
+        './src/styles/custom.css',
+      ],
+      editLink: {
+				baseUrl:
+					"https://github.com/astrolicious/studiocms/tree/main/www/docs",
+			},
       head: [
         {
-          tag: 'script',
+          tag: "script",
           attrs: {
-            src: 'https://analytics.astro-studiocms.xyz/script.js',
-            'data-website-id': 'd9823e76-3219-4f86-9a09-0cb763ebfd19',
+            src: "https://analytics.astro-studiocms.xyz/script.js",
+            "data-website-id": "d9823e76-3219-4f86-9a09-0cb763ebfd19",
             defer: true,
           },
         },
       ],
       sidebar: [
         {
-          label: 'Guides',
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', link: '/guides/example/' },
-          ],
+          label: "Guides",
+          autogenerate: { directory: "guides" },
         },
         {
-          label: 'Reference',
-          autogenerate: { directory: 'reference' },
+          label: "Reference",
+          autogenerate: { directory: "reference" },
         },
       ],
+      lastUpdated: true,
     }),
   ],
-})
+});
