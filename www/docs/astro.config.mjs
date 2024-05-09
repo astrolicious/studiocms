d5f3a5264c501f9f9b20063a5ba5c46fc9f67daa
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2024-05-09 16:34:54 -0600

    add: og banner

--- www/docs/astro.config.mjs
+++ www/docs/astro.config.mjs
@@ -3,9 +3,12 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://docs.astro-studiocms.xyz/",
   integrations: [
     starlight({
       title: "Astro StudioCMS",
+      description:
+        "A dedicated CMS for Astro Studio. Built from the ground up by the Astro community.",
       favicon: "/logo-dark.svg",
       logo: {
         dark: "../assets/logo-light.svg",
@@ -16,12 +19,11 @@ export default defineConfig({
       },
       customCss: [
         // Relative path to your custom CSS file
-        './src/styles/custom.css',
+        "./src/styles/custom.css",
       ],
       editLink: {
-				baseUrl:
-					"https://github.com/astrolicious/studiocms/tree/main/www/docs",
-			},
+        baseUrl: "https://github.com/astrolicious/studiocms/tree/main/www/docs",
+      },
       head: [
         {
           tag: "script",
@@ -29,6 +31,20 @@ export default defineConfig({
             src: "https://analytics.astro-studiocms.xyz/script.js",
             "data-website-id": "d9823e76-3219-4f86-9a09-0cb763ebfd19",
             defer: true,
+          },
+        },
+        {
+          tag: "meta",
+          attrs: {
+            property: "og:image",
+            content: site + "og.png?v=1",
+          },
+        },
+        {
+          tag: "meta",
+          attrs: {
+            property: "twitter:image",
+            content: site + "og.png?v=1",
           },
         },
       ],
