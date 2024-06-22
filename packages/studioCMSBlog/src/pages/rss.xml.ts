2748a926f67d8c3749fbf8fe2f8f8bf099a439eb
Adam Matthiesen <amatthiesen@outlook.com>
2024-06-22 16:58:25 -0700

    use container API to render post content for RSS feed

--- packages/studioCMSBlog/src/pages/rss.xml.ts
+++ packages/studioCMSBlog/src/pages/rss.xml.ts
@@ -1,9 +1,11 @@
 import rss from '@astrojs/rss';
 import type { APIContext } from 'astro';
-import { getPageList, getSiteConfig } from 'studiocms:components';
+import { StudioCMSRenderer, contentHelper, getPageList, getSiteConfig } from 'studiocms:components';
 import config from 'studiocms-blog:config';
 import { pathWithBase } from 'studiocms:helpers';
 import { pages } from 'studiocms-blog:context';
+
+import { experimental_AstroContainer as AstroContainer } from 'astro/container'
 
 const blogRouteFullPath = pages.get('/blog/[...slug]')
 
@@ -32,10 +34,25 @@ export async function GET(context: APIContext) {
 		.filter(page => page.package === "@astrolicious/studiocms-blog")
 		.sort((a, b) => Date.parse(b.publishedAt.toString()) - Date.parse(a.publishedAt.toString()));
 
+	// Render Known Posts
+	const remappedPosts = await Promise.all(orderedPosts.map(async ({slug, title, description, publishedAt, package: pkg}) => {
+		const { content: fetchedContent } = await contentHelper(slug, pkg);
+		const container = await AstroContainer.create();
+		const renderedContent = await container.renderToString(StudioCMSRenderer, {props: { content: fetchedContent }});
+
+		const content = renderedContent
+				.replace(/<!DOCTYPE html>/, '')
+				.replace(/<html.*?>/, '')
+				.replace(/<\/html>/, '')
+				.trim();
+
+		return {slug, title, description, publishedAt, content};
+	}));
+	
 	return rss({
 		title, description, site,
-		items: orderedPosts.map(({ slug, title, description, publishedAt: pubDate }) => ({
-			title, description, pubDate,
+		items: remappedPosts.map(({ slug, title, description, publishedAt: pubDate, content }) => ({
+			title, description, pubDate, content,
 			link: pathWithBase(getBlogRoute(slug)),
 		})),
 	});
