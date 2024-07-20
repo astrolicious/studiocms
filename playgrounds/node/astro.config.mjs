6d747ad08cc67a6d1fc9416f04e1f98586bdfd48
Adam Matthiesen <amatthiesen@outlook.com>
2024-07-20 01:05:00 -0700

    test

--- playgrounds/node/astro.config.mjs
+++ playgrounds/node/astro.config.mjs
@@ -3,7 +3,7 @@ import db from '@astrojs/db';
 import webvitals from '@astrojs/web-vitals';
 import node from '@astrojs/node';
 import studioCMS from '@astrolicious/studiocms';
-import studioCMSBlog from '@astrolicious/studiocms-blog';
+// import studioCMSBlog from '@astrolicious/studiocms-blog';
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,11 +14,11 @@ export default defineConfig({
 		db(), 
 		webvitals(),
 		studioCMS(), // StudioCMS Integration options can be found in `studiocms.config.mjs`
-		studioCMSBlog({
-			config: {
-				title: 'StudioCMS Test Blog',
-				description: 'A simple blog built with Astro and StudioCMS',
-			},
-		}), 
+		// studioCMSBlog({
+		// 	config: {
+		// 		title: 'StudioCMS Test Blog',
+		// 		description: 'A simple blog built with Astro and StudioCMS',
+		// 	},
+		// }), 
 	], 
 });
