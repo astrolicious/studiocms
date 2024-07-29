583f26be0ea36a7e41c982587d14642c8b721ea3
Adam Matthiesen <30383579+Adammatthiesen@users.noreply.github.com>
2024-07-29 11:37:30 -0700

    add demo analytics (#227)

--- playgrounds/node/studiocms.config.mjs
+++ playgrounds/node/studiocms.config.mjs
@@ -7,6 +7,16 @@ export default defineStudioCMSConfig({
 	dateLocale: 'en-us',
 	defaultFrontEndConfig: {
 		favicon: '/favicon.svg',
+		htmlDefaultHead: [
+			{
+				tag: 'script',
+				attrs: {
+					src: 'https://analytics.astro-studiocms.xyz/script.js',
+					'data-website-id': '0d658050-e98a-4c04-8a8f-18af489506c8',
+					defer: true,
+				},
+			},
+		],
 	},
 	includedIntegrations: {
 		useAstroRobots: true,
