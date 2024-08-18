2950c35b91caa592e22056f878553302d8461e1a
Adam Matthiesen <amatthiesen@outlook.com>
2024-08-18 13:18:31 -0700

    lint

--- packages/studioCMS/src/integrations/studioCMSDashboard/utils/webVital.ts
+++ packages/studioCMS/src/integrations/studioCMSDashboard/utils/webVital.ts
@@ -1,5 +1,5 @@
-import { AstrojsWebVitals_Metric, db } from 'astro:db';
 import { logger } from '@it-astro:logger:StudioCMS';
+import { AstrojsWebVitals_Metric, db } from 'astro:db';
 
 export type WebVitalsResponseItem = {
 	id: string;
@@ -14,21 +14,23 @@ export type WebVitalsResponseItem = {
 export async function getWebVitals(): Promise<WebVitalsResponseItem[]> {
 	// Check if AstrojsWebVitals exists
 	if (!AstrojsWebVitals_Metric) {
-		logger.warn("@astrojs/web-vitals table not found.  If you have not installed the package you can disregard this warning.")
+		logger.warn(
+			'@astrojs/web-vitals table not found.  If you have not installed the package you can disregard this warning.'
+		);
 		return [] as WebVitalsResponseItem[];
 	}
 
 	try {
 		// Execute the database query
-		const webVitals: WebVitalsResponseItem[] = await db
-		.select()
-		.from(AstrojsWebVitals_Metric);
+		const webVitals: WebVitalsResponseItem[] = await db.select().from(AstrojsWebVitals_Metric);
 
 		// Return the results if successful
 		return webVitals;
 	} catch (error) {
 		// Log the error for debugging purposes
-		logger.error(`Error getting @astrojs/web-vitals table data.  If you have not installed the package you can disregard this error.\n - ${error}`);
+		logger.error(
+			`Error getting @astrojs/web-vitals table data.  If you have not installed the package you can disregard this error.\n - ${error}`
+		);
 		return [] as WebVitalsResponseItem[];
 	}
 }
