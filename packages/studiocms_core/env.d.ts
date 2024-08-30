25714b77917d9887bbed2d6375f169c0f284d1e8
Adam Matthiesen <amatthiesen@outlook.com>
2024-08-30 05:49:44 -0700

    chore: Add App.Locals interface to env.d.ts in @studiocms/core

--- packages/studiocms_core/env.d.ts
+++ packages/studiocms_core/env.d.ts
@@ -6,3 +6,36 @@ interface ImportMetaEnv {
 interface ImportMeta {
 	readonly env: ImportMetaEnv;
 }
+
+declare namespace App {
+	interface Locals {
+		isLoggedIn: boolean;
+		dbUser?: {
+			id: string;
+			url: string | null;
+			name: string;
+			email: string | null;
+			avatar: string | null;
+			githubId: number | null;
+			githubURL: string | null;
+			discordId: string | null;
+			googleId: string | null;
+			auth0Id: string | null;
+			username: string;
+			password: string | null;
+			updatedAt: Date | null;
+			createdAt: Date | null;
+		} | null;
+		user?: {
+			id: string;
+			username?: string;
+			githubId?: number;
+		} | null;
+		session?: {
+			id: string;
+			userId: string;
+			fresh: boolean;
+			expiresAt: Date;
+		} | null;
+	}
+}
