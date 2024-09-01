1e389a39f473037a254c0615f5641ea103c04838
Adam Matthiesen <amatthiesen@outlook.com>
2024-09-01 04:57:12 -0700

    update

--- packages/studiocms_core/src/index.ts
+++ packages/studiocms_core/src/index.ts
@@ -1,14 +1,6 @@
 // Import core-integration
 import integration from './integration';
 
-// Export re-usables
-// export * from './lib';
-// export * from './schemas';
-// export * from './types';
-// export * from './utils';
-// export * from './strings';
-// export * from './consts';
-
 /**
  * Core StudioCMS Integration
  */
