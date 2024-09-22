231aa77720846090af10dbcd8ebc9b4a142aaab2
Adam Matthiesen <amatthiesen@outlook.com>
2024-09-22 08:13:38 -0700

    add roadmap redirect page

--- www/web/src/constants.ts
+++ www/web/src/constants.ts
@@ -14,5 +14,7 @@ export const GITHUB_URL = 'https://github.com/astrolicious/studiocms';
 export const SPONSOR_URL = 'https://github.com/astrolicious/studiocms';
 export const DISCORD_URL = 'https://chat.studiocms.xyz/';
 export const DOCS_URL = 'https://docs.studiocms.xyz/';
+export const ROADMAP_URL =
+	'https://roadmap.studiocms.xyz/spaces/issues/e0f1c24900e44954ba3d93e46d8d8b9d/?board=kanban';
 
 // Testimonials and Features have been moved to content collections
