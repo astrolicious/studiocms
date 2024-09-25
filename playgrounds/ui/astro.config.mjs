c0f228f7296996e99e6b5451db0eb3e0ce28c974
Louis Escher <louisescher@proton.me>
2024-09-25 18:39:46 +0200

    Update & dedupe packages

--- playgrounds/ui/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import node from '@astrojs/node';
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-	site: 'http://localhost:4321/',
-	output: 'server',
-	adapter: node({ mode: 'standalone' }),
-});
---
+++ playgrounds/ui/astro.config.mts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://ui-testing.studiocms.xyz',
+	output: 'static',
+});
