7b376b7e16340fad410d678b1f3a0d4d765e0521
Adam Matthiesen <amatthiesen@outlook.com>
2024-09-30 21:04:54 -0700

    test

--- www/web/src/pages/blog/[...slug]/og.png.ts
+++ www/web/src/pages/blog/[...slug]/og.png.ts
@@ -27,7 +27,7 @@ export const GET: APIRoute = async ({ props }: APIContext) => {
 
 	return await satoriAstroOG({
 		template: html`<div style='width: 100%; height: 100%; position: relative; display:flex'>
-				<img src='${ogBG}' width="3840" height="2160" style='width: 100%; height: 100%; object-fit: cover; display: flex;' />
+				<img src=${ogBG} width="3840" height="2160" style='width: 100%; height: 100%; object-fit: cover; display: flex;' />
 				<div style="left: 50%; top: 45%; transform: translate(-50%, -50%); position: absolute; color: white; font-size: 250px; font-family: Onest; font-weight: 700; word-wrap: break-word; display: flex; flex-direction: column; justify-items: center; align-items: center;">StudioCMS
 				<p style="font-size: 100px; margin: 0; font-family: Onest; font-weight: 400;">${post.data.description}</p>
 				</div></div>`,
