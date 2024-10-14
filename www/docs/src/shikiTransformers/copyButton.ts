aec44e82ac30f620e2c8d6f779e848a357ce94d2
Adam Matthiesen <amatthiesen@outlook.com>
2024-10-14 11:22:49 -0700

    update css

--- www/docs/src/shikiTransformers/copyButton.ts
+++ www/docs/src/shikiTransformers/copyButton.ts
@@ -28,7 +28,7 @@ export function addCopyButton(options?: { toggle?: number }): ShikiTransformer {
 							[
 								h('path', {
 									d: 'M9 18q-.825 0-1.413-.588T7 16V4q0-.825.588-1.413T9 2h9q.825 0 1.413.588T20 4v12q0 .825-.588 1.413T18 18H9Zm0-2h9V4H9v12Zm-4 6q-.825 0-1.413-.588T3 20V7q0-.425.288-.713T4 6q.425 0 .713.288T5 7v13h10q.425 0 .713.288T16 21q0 .425-.288.713T15 22H5Zm4-6V4v12Z',
-									fill: 'var(--sl-color-gray-3)',
+									fill: 'var(--copy-button)',
 								}),
 							]
 						),
@@ -40,7 +40,7 @@ export function addCopyButton(options?: { toggle?: number }): ShikiTransformer {
 							[
 								h('path', {
 									d: 'm10.6 16.2l7.05-7.05l-1.4-1.4l-5.65 5.65l-2.85-2.85l-1.4 1.4l4.25 4.25ZM5 21q-.825 0-1.413-.588T3 19V5q0-.825.588-1.413T5 3h14q.825 0 1.413.588T21 5v14q0 .825-.588 1.413T19 21H5Z',
-									fill: '#00c200',
+									fill: 'var(--copy-button-success)',
 								}),
 							]
 						),
