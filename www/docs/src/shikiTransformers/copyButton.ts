041d35f5429000c2348a28f0518ddfab8cbe2c1b
Adam Matthiesen <amatthiesen@outlook.com>
2024-10-14 11:10:33 -0700

    some css styling for the copy button

--- www/docs/src/shikiTransformers/copyButton.ts
+++ www/docs/src/shikiTransformers/copyButton.ts
@@ -28,7 +28,7 @@ export function addCopyButton(options?: { toggle?: number }): ShikiTransformer {
 							[
 								h('path', {
 									d: 'M9 18q-.825 0-1.413-.588T7 16V4q0-.825.588-1.413T9 2h9q.825 0 1.413.588T20 4v12q0 .825-.588 1.413T18 18H9Zm0-2h9V4H9v12Zm-4 6q-.825 0-1.413-.588T3 20V7q0-.425.288-.713T4 6q.425 0 .713.288T5 7v13h10q.425 0 .713.288T16 21q0 .425-.288.713T15 22H5Zm4-6V4v12Z',
-									fill: 'var(--sl-color-white)',
+									fill: 'var(--sl-color-gray-3)',
 								}),
 							]
 						),
