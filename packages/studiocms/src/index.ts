98458aa495535ec6a79ada8387ecead8deaafb01
Adam Matthiesen <amatthiesen@outlook.com>
2024-10-14 02:31:01 -0700

    Refactor StudioCMS package.json exports

--- packages/studiocms/src/index.ts
+++ packages/studiocms/src/index.ts
@@ -2,6 +2,17 @@ import { defineStudioCMSConfig, defineStudioCMSPlugin } from '@studiocms/core/li
 import type { StudioCMSPluginOptions } from '@studiocms/core/types';
 import integration from './integration';
 
+/**
+ * **StudioCMS Integration**
+ *
+ * A CMS built for Astro by the Astro Community for the Astro Community.
+ *
+ * > **Note: Astro SSR adapters that are configured for Image Optimization will automatically take full control of the Image Optimization Service. Making the `imageService` option in this integration not have any effect.**
+ *
+ * @see [GitHub Repo: 'astrolicious/studiocms'](https://github.com/astrolicious/studiocms) for more information on how to contribute to StudioCMS.
+ * @see [StudioCMS Docs](https://docs.studiocms.xyz) for more information on how to use StudioCMS.
+ *
+ */
 export const studioCMS = integration;
 
 export default studioCMS;
